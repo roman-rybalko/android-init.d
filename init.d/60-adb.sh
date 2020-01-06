5fdfd7dba6da8a491d0d780fc78753c46e5b4ac7
Roman Rybalko <devel@romanr.info>
2020-01-06 07:06:15 +0300

    adb: block IPv6

--- init.d/60-adb.sh
+++ init.d/60-adb.sh
@@ -4,6 +4,10 @@
 set -x
 set -e
 
+# IPv6 provides a global address.
+# Prevent exposing adbd port to the internet.
+ip6tables -I INPUT -p tcp --dport 5555 -j REJECT
+
 setprop persist.internet.adb.enable 0
 {
 sleep 1
