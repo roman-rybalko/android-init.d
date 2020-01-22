eca908b0577df0649021dac6ad2c968495946c66
Roman Rybalko <devel@romanr.info>
2020-01-22 22:01:40 +0300

    network watchdog: timestamp, ipsec state flush

--- init.d/50-net-wd.sh
+++ init.d/50-net-wd.sh
@@ -4,9 +4,12 @@ IF=eth0
 
 while true; do
 	if ! ifconfig $IF | grep -E 'inet.? addr'; then
+		date
 		ifconfig $IF down
 		sleep 10
 		ifconfig $IF up
+		sleep 10
+		ip xfrm state flush
 	fi
 	sleep 600
 done &
